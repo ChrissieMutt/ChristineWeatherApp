1ac864e01bc153be874be79b5053a2bc7fd11cb7
ChrissieMutt <christinemutseura@gmail.com>
2024-09-19 04:10:06 -0700

    updated java script file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "f91o7at26fc6a5a0df4bb1a219423ab4";
+  let apiURL =
+    'https://api.shecodes.io/weather/vl/current?query=${city}&key=$(apiKey}$units=metric"';
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  seachCity(searchInput, value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
